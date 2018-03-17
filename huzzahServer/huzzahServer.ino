e1ac24e3b0e34cc28132993f09ac0493c93052e3
Hariharan <theroboticlabs@gmail.com>
2018-03-18 00:08:17 +0530

    Ported the detector code to Huzzah.

--- huzzahServer/huzzahServer.ino
+++ huzzahServer/huzzahServer.ino
@@ -2,16 +2,30 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include<SoftwareSerial.h>
+#include<NewPing.h>
+
+NewPing left(14,12); //Trigger and echo
+NewPing right(13,15);
+const int rx = 2;
+const int tx = 3;
+long durationL, durationR;
+const int echoL = 12;
+const int echoR = 11;
+const int trigL = 9;
+const int trigR = 8;
+#define SPEED 343
 const char* ssid = "AndroidAP";
 const char* password = "coehv1902ha,vj";
 
+SoftwareSerial btserial(2,16);
 ESP8266WebServer server(80); 
 
 void handleRoot() {
- String s = "Distance: 23"; 
+ String s = "DurationL: "+durationL+" DurationR: "+durationR; 
  server.send(200, "text/html", s); 
  
-void setup(void){
+void setup(){
   Serial.begin(9600);
   
   WiFi.begin(ssid, password);     
@@ -24,6 +38,12 @@ void setup(void){
   server.begin();                  
 }
 
-void loop(void){
-  server.handleClient();          
+void loop(){
+  server.handleClient();  
+  btserial.write(2);
+  durationL = left.ping();
+  durationR = right.ping();
+  Serial.println("L "+(String)distL+ " R "+(String)distR);
+  //Serial.println(distR);
+  delay(100);        
 }
