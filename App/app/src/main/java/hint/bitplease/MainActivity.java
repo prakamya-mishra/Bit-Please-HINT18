db96b01b40a6a0dbec9f8583d91fba159ac94c78
sd1998 <shashvat51@gmail.com>
2018-03-17 13:16:33 +0530

    Firebase JSON structure changed

--- App/app/src/main/java/hint/bitplease/MainActivity.java
+++ App/app/src/main/java/hint/bitplease/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
@@ -12,13 +13,20 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = MainActivity.class.getName();
 
     private TextView txtSpeechInput;
     private EditText Distance;
@@ -43,10 +51,7 @@ public class MainActivity extends AppCompatActivity {
                 promptSpeechInput();
             }
         });
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        parentRef = firebaseDatabase.getReference();
-        sensorDataRef = parentRef.child("sensorinfo");
-        sensorDataRef.setValue(new SensorData(1.0f,1.0f,1.0f));
+        writeValuesToFirebase();
     }
 
     private void promptSpeechInput() {
@@ -83,4 +88,63 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void writeValuesToFirebase(){
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        parentRef = firebaseDatabase.getReference();
+        sensorDataRef = parentRef.child("sensorinfo");
+        DatabaseReference distanceRef = sensorDataRef.child("distance");
+        DatabaseReference gyroXRef = sensorDataRef.child("gyroX");
+        DatabaseReference gyroYRef = sensorDataRef.child("gyroY");
+        DatabaseReference gyroZRef = sensorDataRef.child("gyroZ");
+        DatabaseReference distanceLeft = sensorDataRef.child("distanceLeft");
+        DatabaseReference distanceRight = sensorDataRef.child("distanceRight");
+        distanceLeft.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                GenericTypeIndicator<List<String>> genericTypeIndicator = new GenericTypeIndicator<List<String>>();
+                List distanceLeft = dataSnapshot.getValue(genericTypeIndicator);
+                if(distanceLeft == null){
+                    Log.d(TAG,"Data error");
+                }
+                else{
+                    if(distanceLeft.size() == 10){
+                        distanceLeft.remove(0);
+                        distanceLeft.add(new Float(1.0));
+                    }
+                    else{
+                        distanceLeft.add(new Float(1.0));
+                    }
+                    
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d(TAG,databaseError.getCode() + databaseError.getMessage());
+            }
+        });
+        distanceRight.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                GenericTypeIndicator<List<String>> genericTypeIndicator = new GenericTypeIndicator<List<String>>();
+                List distanceRight = dataSnapshot.getValue(genericTypeIndicator);
+                if(distanceRight == null){
+                    Log.d(TAG,"Data error");
+                }
+                else{
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d(TAG,databaseError.getCode() + databaseError.getMessage());
+            }
+        });
+        distanceRef.setValue(new Float(1.0));
+        gyroXRef.setValue(new Float(1.0));
+        gyroYRef.setValue(new Float(1.0));
+        gyroZRef.setValue(new Float(1.0));
+        sensorDataRef.setValue(new SensorData(1.0f,1.0f,1.0f,1.0f,1.0f,1.0f));
+    }
 }
