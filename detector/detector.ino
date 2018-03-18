8899e0796007eec8ab697cb9c6222dfd142f88b2
Hariharan <theroboticlabs@gmail.com>
2018-03-18 07:39:58 +0530

    Bug fixes for motor control.

--- detector/detector.ino
+++ detector/detector.ino
@@ -1,8 +1,8 @@
 #include<SoftwareSerial.h>
 #include<NewPing.h>
 
-NewPing left(12,9);
-NewPing right(7,8);
+NewPing left(12,9,200);
+NewPing right(7,8,200);
 const int rx = 2;
 const int tx = 3;
 long durationL, durationR;
@@ -31,21 +31,21 @@ void loop() {
   
  // btserial.write(2);
   //Serial.println("1");
-  btserial.write(2);
-  long distL = left.ping();
-  long distR = right.ping();
-  //Serial.println("L "+(String)distL+ " R "+(String)distR);
-  //Serial.println(distR);
-  delay(100);
-  motorL = map(distL, 0, 15000, 0, 255);
-  motorR = map(distR, 0, 15000, 0, 255);
-  analogWrite(ENR, motorR);
-  analogWrite(ENL, motorL);
-  digitalWrite(motorL1, HIGH);
-  digitalWrite(motorL2, LOW);
-  digitalWrite(motorR1, LOW);
-  digitalWrite(motorR2, HIGH);
-  Serial.println("L: "+(String)motorL+" R: "+(String)motorR);
+    btserial.write(2);
+    long distL = left.ping();
+    long distR = right.ping();
+    //Serial.println("L "+(String)distL+ " R "+(String)distR);
+    //Serial.println(distR);
+    delay(100);
+    motorL = map(distL, 0, 2000, 0, 255);
+    motorR = map(distR, 0, 2000, 0, 255);
+    analogWrite(ENR, motorR);
+    analogWrite(ENL, motorL);
+    digitalWrite(motorL1, HIGH);
+    digitalWrite(motorL2, LOW);
+    digitalWrite(motorR1, LOW);
+    digitalWrite(motorR2, HIGH);
+    Serial.println("L: "+(String)motorL+" R: "+(String)motorR);
   //btserial.write("0");
   //delay(2000);
   /*if(Serial.available())
