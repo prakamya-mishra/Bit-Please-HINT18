99a813788e52b83f5fa0ea67e3ad5ae5dc679d6b
Hariharan <theroboticlabs@gmail.com>
2018-03-18 06:59:10 +0530

    Added motor control.

--- detector/detector.ino
+++ detector/detector.ino
@@ -10,6 +10,8 @@ const int echoL = 12;
 const int echoR = 11;
 const int trigL = 9;
 const int trigR = 8;
+int motorL = 0;
+int motorR = 0;
 #define SPEED 343
 
 SoftwareSerial btserial(rx,tx);
@@ -30,10 +32,12 @@ void loop() {
   btserial.write(2);
   long distL = left.ping();
   long distR = right.ping();
-  Serial.println("L "+(String)distL+ " R "+(String)distR);
+  //Serial.println("L "+(String)distL+ " R "+(String)distR);
   //Serial.println(distR);
   delay(100);
-  
+  motorL = map(distL, 0, 15000, 0, 255);
+  motorR = map(distR, 0, 15000, 0, 255);
+  Serial.println("L: "+(String)motorL+" R: "+(String)motorR);
   //btserial.write("0");
   //delay(2000);
   /*if(Serial.available())
