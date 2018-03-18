ac1e2fe060dd1bbd692ba3e208ae27a4de182677
Hariharan <theroboticlabs@gmail.com>
2018-03-18 08:05:36 +0530

    Added digital motor control.

--- detector/detector.ino
+++ detector/detector.ino
@@ -32,20 +32,38 @@ void loop() {
  // btserial.write(2);
   //Serial.println("1");
     btserial.write(2);
-    long distL = left.ping();
+    long distL = left.ping(); //Time to reach the sensor
     long distR = right.ping();
     //Serial.println("L "+(String)distL+ " R "+(String)distR);
     //Serial.println(distR);
-    delay(100);
-    motorL = map(distL, 0, 2000, 0, 255);
+    
+    /*motorL = map(distL, 0, 2000, 0, 255);
     motorR = map(distR, 0, 2000, 0, 255);
     analogWrite(ENR, motorR);
     analogWrite(ENL, motorL);
     digitalWrite(motorL1, HIGH);
     digitalWrite(motorL2, LOW);
     digitalWrite(motorR1, LOW);
+    digitalWrite(motorR2, HIGH);*/
+    //Serial.println("L: "+(String)motorL+" R: "+(String)motorR);
+/********************************************************************************
+ * Digital Motor Control
+ ********************************************************************************/
+
+    while(distL > distR)
+    {
+      digitalWrite(motorL1, HIGH);
+      digitalWrite(motorL2, LOW);
+    }
+    while(distR > distL)
+    {
+      digitalWrite(motorR1, HIGH);
+      digitalWrite(motorR2, LOW);
+    }
+    digitalWrite(motorL1, HIGH);
+    digitalWrite(motorL2, LOW);
+    digitalWrite(motorR1, LOW);
     digitalWrite(motorR2, HIGH);
-    Serial.println("L: "+(String)motorL+" R: "+(String)motorR);
   //btserial.write("0");
   //delay(2000);
   /*if(Serial.available())
@@ -58,5 +76,6 @@ void loop() {
     char c = Serial.read();
     Serial.write(c);
   }*/
+    delay(100);
     
 }
